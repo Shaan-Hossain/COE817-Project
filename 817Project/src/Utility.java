7d7c51784ff665cdca3e5aea1678e0a8ac9dea4e
Darien Lee <43325791+Pilloww@users.noreply.github.com>
2022-03-30 22:21:23 -0400

    minimal changes

--- 817Project/src/Utility.java
+++ 817Project/src/Utility.java
@@ -19,39 +19,29 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Utility {
-	public static SecretKey getKeyFromPassword(String password, String salt)
-		    throws NoSuchAlgorithmException, InvalidKeySpecException {
-		    
+	public static SecretKey genKeyWithSalt(String password, String salt) throws NoSuchAlgorithmException, InvalidKeySpecException {   
 		    SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
 		    KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
-		    SecretKey secret = new SecretKeySpec(factory.generateSecret(spec)
-		        .getEncoded(), "AES");
+		    SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
 		    return secret;
 	}
 	
 	public static String encrypt(String encryptedMessage, SecretKey SecKey_gen, IvParameterSpec iv) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException, InvalidAlgorithmParameterException {
-		Cipher DES_Cipher2 = Cipher.getInstance("AES/CBC/PKCS5Padding");
-       
-		DES_Cipher2.init(Cipher.ENCRYPT_MODE, SecKey_gen, iv);              
-        byte[] Msg_ByteCode = DES_Cipher2.doFinal(encryptedMessage.getBytes("UTF-8"));
-        
-        String enc_Msg = Base64.getEncoder().encodeToString(Msg_ByteCode);
-        
-		return enc_Msg;
+		Cipher cipher_DES = Cipher.getInstance("AES/CBC/PKCS5Padding");       
+		cipher_DES.init(Cipher.ENCRYPT_MODE, SecKey_gen, iv);              
+        byte[] byte_msg = cipher_DES.doFinal(encryptedMessage.getBytes("UTF-8"));      
+        String encoded_msg = Base64.getEncoder().encodeToString(byte_msg);    
+		return encoded_msg;
 	}
 	
     public static String decrypt(String encryptedMessage, SecretKey SecKey_gen, IvParameterSpec iv) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
-        String en_Msg = encryptedMessage;
-
-        Cipher DES_Cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        DES_Cipher.init(Cipher.DECRYPT_MODE, SecKey_gen, iv);
-
-        byte[] decByteCode = Base64.getDecoder().decode(en_Msg);
-        byte[] deEncByte = DES_Cipher.doFinal(decByteCode);
-
-        String dec_FinalMsg = new String(deEncByte);
-
-        return dec_FinalMsg;
+        String encrypted_msg = encryptedMessage;
+        Cipher cipher_DES = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher_DES.init(Cipher.DECRYPT_MODE, SecKey_gen, iv);
+        byte[] decode_byte = Base64.getDecoder().decode(encrypted_msg);
+        byte[] decrypt_byte = cipher_DES.doFinal(decode_byte);
+        String decoded_msg = new String(decrypt_byte);
+        return decoded_msg;
     }
 	
 }
